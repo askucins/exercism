8e9df3e1104618df4fa18911721426eb39b50a68
Adam Skucinski <askucins@gmail.com>
2019-06-17 22:49:06 +0200

    another solution for isogram

--- groovy/isogram/src/main/groovy/Isogram.groovy
+++ groovy/isogram/src/main/groovy/Isogram.groovy
@@ -1,5 +1,24 @@
 class Isogram {
     static boolean isIsogram(String phrase) {
+        def tokens = phrase.toList()*.toLowerCase()
+        Set<String> uniqueTokens = []
+        def result = true
+        while (tokens.size() > 0 && result) {
+            def token = tokens.pop()
+            if ([' ', '-'].contains(token)) {
+                continue
+            }
+            if (uniqueTokens.contains(token)) {
+                result = false
+            } else {
+                uniqueTokens.add(token)
+            }
+        }
+        return result
+    }
+
+    // First approach, not that very nice
+    static boolean initialVersion(String phrase) {
         def graph = [:]
         for (token in phrase) {
             if ([' ', '-'].contains(token)) {
