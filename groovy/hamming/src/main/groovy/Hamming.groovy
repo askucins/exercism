186fc9020abac2e7da043e95acd90029476fba22
Adam Skucinski <askucins@gmail.com>
2019-06-21 18:59:40 +0200

    improved hamming based on community solution

--- groovy/hamming/src/main/groovy/Hamming.groovy
+++ groovy/hamming/src/main/groovy/Hamming.groovy
@@ -7,7 +7,10 @@ class Hamming {
         if (strand1.size() == 0) {
             return 0
         } else {
-            [strand1.collect(), strand2.collect()].transpose().collect { it[0] == it[1] ? 0 : 1 }.sum()
+            // Mine
+            //[strand1.collect(), strand2.collect()].transpose().collect { it[0] == it[1] ? 0 : 1 }.sum()
+            // Based on community solution - it's better to count them!
+            [strand1.collect(), strand2.collect()].transpose().count { st1, st2 -> st1 != st2 }
         }
     }
 }
