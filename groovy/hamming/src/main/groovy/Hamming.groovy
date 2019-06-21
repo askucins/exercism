6a95e45a90f4702a27699aedae2f14fe5d45abfb
Adam Skucinski <askucins@gmail.com>
2019-06-21 18:53:11 +0200

    passed hamming

--- groovy/hamming/src/main/groovy/Hamming.groovy
+++ groovy/hamming/src/main/groovy/Hamming.groovy
@@ -1,7 +1,13 @@
 class Hamming {
 
-    def distance(strand1, strand2) {
-        throw new UnsupportedOperationException('Method implementation is missing')
+    static distance(String strand1, String strand2) {
+        if (strand1.size() != strand2.size()) {
+            throw new ArithmeticException()
+        }
+        if (strand1.size() == 0) {
+            return 0
+        } else {
+            [strand1.collect(), strand2.collect()].transpose().collect { it[0] == it[1] ? 0 : 1 }.sum()
+        }
     }
-
 }
