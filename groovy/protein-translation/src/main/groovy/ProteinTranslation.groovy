7f21001886b40529395f05f1d090fb1fc3667621
Adam Skucinski <askucins@gmail.com>
2019-06-21 15:16:00 +0200

    passed protein-translation (basic approach)

--- groovy/protein-translation/src/main/groovy/ProteinTranslation.groovy
+++ groovy/protein-translation/src/main/groovy/ProteinTranslation.groovy
@@ -1,6 +1,53 @@
 class ProteinTranslation {
 
-    static proteins(strand) {
-        throw new UnsupportedOperationException('Method implementation is missing')
+    static STOP = 'STOP'
+    enum AminoAcid {
+        Methionine,
+        Phenylalanine,
+        Leucine,
+        Serine,
+        Tyrosine,
+        Cysteine,
+        Tryptophan,
+        // Control sequence
+        STOP
+    }
+
+    static translate = [
+            AUG: AminoAcid.Methionine,
+            UUU: AminoAcid.Phenylalanine,
+            UUC: AminoAcid.Phenylalanine,
+            UUA: AminoAcid.Leucine,
+            UUG: AminoAcid.Leucine,
+            UCU: AminoAcid.Serine,
+            UCC: AminoAcid.Serine,
+            UCA: AminoAcid.Serine,
+            UCG: AminoAcid.Serine,
+            UAU: AminoAcid.Tyrosine,
+            UAC: AminoAcid.Tyrosine,
+            UGU: AminoAcid.Cysteine,
+            UGC: AminoAcid.Cysteine,
+            UGG: AminoAcid.Tryptophan,
+            UAA: AminoAcid.STOP,
+            UAG: AminoAcid.STOP,
+            UGA: AminoAcid.STOP
+    ]
+
+    static proteins(String strand) {
+        def result = []
+        boolean continueTranslation = true
+        while (strand && continueTranslation) {
+            def codon = strand.substring(0, 3)
+            assert codon.size() == 3: 'Missing nucleotides!'
+            def translation = translate[(codon)].toString()
+            assert translation: 'Not recognized codon!'
+            if (translation == AminoAcid.STOP.toString()) {
+                continueTranslation = false
+            } else {
+                result.add translation
+                strand = strand.substring(3)
+            }
+        }
+        result
     }
 }
