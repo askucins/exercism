2f11ac392b9388d67328dae9d542787e23f2cebf
Adam Skucinski <askucins@gmail.com>
2019-06-22 10:44:34 +0200

    misc changes based on community solutions

--- groovy/difference-of-squares/src/main/groovy/DifferenceOfSquares.groovy
+++ groovy/difference-of-squares/src/main/groovy/DifferenceOfSquares.groovy
@@ -7,11 +7,13 @@ class DifferenceOfSquares {
 
     def squareOfSum() {
         def sum = (1..num).sum(0)
-        sum * sum
+        //sum * sum
+        sum.power(2)
     }
 
     def sumOfSquares() {
-        (1..num).collect { it * it }.sum(0)
+        //(1..num).collect { it * it }.sum(0)
+        (1..num)*.power(2).sum(0)
     }
 
     def difference() {
