26c47f29a09d76aa3dbbc081de84575453176ad1
Adam Skucinski <askucins@gmail.com>
2019-06-22 10:10:41 +0200

    passed scrabble-score

--- groovy/scrabble-score/src/main/groovy/ScrabbleScore.groovy
+++ groovy/scrabble-score/src/main/groovy/ScrabbleScore.groovy
@@ -1,7 +1,30 @@
 class ScrabbleScore {
 
+    static Integer weight(character) {
+        Integer result
+        switch (character.toUpperCase()) {
+            case ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T']:
+                result = 1; break
+            case ['D', 'G']:
+                result = 2; break
+            case ['B', 'C', 'M', 'P']:
+                result = 3; break
+            case ['F', 'H', 'V', 'W', 'Y']:
+                result = 4; break
+            case ['K']:
+                result = 5; break
+            case ['J', 'X']:
+                result = 8; break
+            case ['Q', 'Z']:
+                result = 10; break
+            default:
+                throw new IllegalArgumentException("This is not a character: $character")
+        }
+        result
+    }
+
     static scoreWord(String word) {
-        throw new UnsupportedOperationException('scoreWord method not implemented.')
+        word.collect { weight(it) }.sum() ?: 0
     }
 
 }
