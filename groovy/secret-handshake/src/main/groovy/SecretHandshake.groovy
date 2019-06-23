9a5df7a20744370f6e89492a62bfbe3eaf81dab2
Adam Skucinski <askucins@gmail.com>
2019-06-23 15:35:55 +0200

    passed secret-handshake

--- groovy/secret-handshake/src/main/groovy/SecretHandshake.groovy
+++ groovy/secret-handshake/src/main/groovy/SecretHandshake.groovy
@@ -1,6 +1,21 @@
 class SecretHandshake {
 
+    static handshakes = [
+            ['wink', 0b1],
+            ['double blink', 0b10],
+            ['close your eyes', 0b100],
+            ['jump', 0b1000]
+    ]
+    static int reverse = 0b10000
+    static int mask = 0b11111
+
     static List<String> commands(int number) {
-        throw new UnsupportedOperationException('method not implemented.')
+        number = number & mask
+        List<String> result = handshakes.grep { (number & it[1]) }.collect { it[0] }
+        if (number & reverse) {
+            return result.reverse()
+        } else {
+            return result
+        }
     }
 }
