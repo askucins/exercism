7cdef55cb7b29d97d55dc75d4cb9954f5ccb2f05
Adam Skucinski <askucins@gmail.com>
2019-06-23 21:43:48 +0200

    passed robot-name

--- groovy/robot-name/src/main/groovy/RobotName.groovy
+++ groovy/robot-name/src/main/groovy/RobotName.groovy
@@ -3,7 +3,23 @@ class RobotName {
     String name
 
     RobotName() {
-        throw new UnsupportedOperationException('Constructor implementation is missing')
+        name = randomName()
     }
 
+    def reset() {
+        name = randomName()
+    }
+
+    static String randomName() {
+        def letters = ('A'..'Z')
+        def numbers = (0..9)
+        def getRandomFrom = { List family ->
+            Integer index = Math.floor(Math.random() * family.size()).toInteger()
+            family[(index)]
+        }
+        String result = ''
+        2.times { result += getRandomFrom(letters) }
+        3.times { result += getRandomFrom(numbers) }
+        result
+    }
 }
