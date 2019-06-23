42562eb8f887cf4b6ca3bfb02190e7a92e14bb17
Adam Skucinski <askucins@gmail.com>
2019-06-23 15:46:54 +0200

    misc refactoring

--- groovy/secret-handshake/src/main/groovy/SecretHandshake.groovy
+++ groovy/secret-handshake/src/main/groovy/SecretHandshake.groovy
@@ -7,14 +7,12 @@ class SecretHandshake {
             [0b1000, 'jump']
     ]
     static int reverse = 0b10000
-    static int mask = 0b11111
 
     static List<String> commands(int number) {
-        number = number & mask // We care only about 'handshake-like' numbers
-
-        List<String> handshake = handshakes.grep { (number & it[0]) }.collect { it[1].toString() }
+        List<String> handshake = handshakes
+                .grep { (number & it[0]) }
+                .collect { it[1].toString() }
 
         (number & reverse) ? handshake.reverse() : handshake
-
     }
 }
