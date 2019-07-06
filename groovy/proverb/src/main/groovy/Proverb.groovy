e01453b871e3ce8fe18a7687d3c851aa2c2e5cb6
Adam Skucinski <askucins@gmail.com>
2019-07-06 23:55:35 +0200

    passed tests of proverb

--- groovy/proverb/src/main/groovy/Proverb.groovy
+++ groovy/proverb/src/main/groovy/Proverb.groovy
@@ -1,6 +1,31 @@
 class Proverb {
 
-    static recite(strings) {
-        throw new UnsupportedOperationException('Method implementation is missing')
+    static String lastSentence(String stuff) {
+        "And all for the want of a $stuff."
+    }
+
+    static String runningSentence(String first, String second) {
+        "For want of a $first the $second was lost.\n"
+    }
+
+
+    static String reciteHelper(List<String> strings, String accu) {
+        switch (strings.size()) {
+            case 1:
+                return accu
+            default:
+                String first = strings.pop()
+                String second = strings.first()
+                return reciteHelper(strings, accu + runningSentence(first, second))
+        }
+    }
+
+    static String recite(List<String> strings) {
+        String result = ''
+        if (strings.size()) {
+            String lastSentence = lastSentence(strings.first())
+            result = reciteHelper(strings, '') + lastSentence
+        }
+        result
     }
 }
