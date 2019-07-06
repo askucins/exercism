8caf895f010f681cceaf55e1a48829325dc91718
Adam Skucinski <askucins@gmail.com>
2019-07-07 00:19:07 +0200

    refactored proverb

--- groovy/proverb/src/main/groovy/Proverb.groovy
+++ groovy/proverb/src/main/groovy/Proverb.groovy
@@ -7,24 +7,20 @@ class Proverb {
     static String runningSentence(String first, String second) {
         "For want of a $first the $second was lost.\n"
     }
-
-
-    static String reciteHelper(List<String> strings, String accu) {
-        switch (strings.size()) {
-            case 1:
-                return accu
-            default:
-                String first = strings.pop()
-                String second = strings.first()
-                return reciteHelper(strings, accu + runningSentence(first, second))
-        }
-    }
-
+    
     static String recite(List<String> strings) {
         String result = ''
-        if (strings.size()) {
-            String lastSentence = lastSentence(strings.first())
-            result = reciteHelper(strings, '') + lastSentence
+        switch (strings.size()) {
+            case 0:
+                break
+            case 1:
+                result = lastSentence(strings.first())
+                break
+            default:
+                (0..strings.size() - 2).each {
+                    result += runningSentence(strings[(it)], strings[(it + 1)])
+                }
+                result += lastSentence(strings.first())
         }
         result
     }
