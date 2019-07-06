a0b647400d4440c93b85f48ead15a964c1d57235
Adam Skucinski <askucins@gmail.com>
2019-07-06 12:17:02 +0200

    changed pangram tests based on community solutions

--- groovy/pangram/src/main/groovy/Pangram.groovy
+++ groovy/pangram/src/main/groovy/Pangram.groovy
@@ -1,7 +1,9 @@
 class Pangram {
 
     static boolean isPangram(String sentence) {
-        def sentenceSummary = sentence.toLowerCase().replaceAll(/[^a-z]/, '').collect().sort().unique()
-        ('a'..'z') == sentenceSummary
+        //def sentenceSummary = sentence.toLowerCase().replaceAll(/[^a-z]/, '').collect().sort().unique()
+        // Based on community solutions:
+        def sentenceSummary = sentence.toLowerCase().replaceAll(/[^a-z]/, '').collect()
+        ('a'..'z') as Set == sentenceSummary as Set
     }
 }
