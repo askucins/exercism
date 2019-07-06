7587bc20a207c069eefc34b7f402e0dacf143c0a
Adam Skucinski <askucins@gmail.com>
2019-07-06 13:17:01 +0200

    passed tests of acronym

--- groovy/acronym/src/main/groovy/Acronym.groovy
+++ groovy/acronym/src/main/groovy/Acronym.groovy
@@ -1,7 +1,17 @@
 class Acronym {
 
     static String abbreviate(String phrase) {
-        throw new UnsupportedOperationException('method not implemented.')
+        initialSolution(phrase)
     }
 
+    static String initialSolution(String phrase) {
+        phrase
+                .toLowerCase()
+                .replaceAll(/'/, '') //apostrophes do not generate separate words
+                .replaceAll(/[^a-z]/, ' ') // all other non-letters do
+                .split(/\s+/)
+                .collect { it[0] }
+                .join()
+                .toUpperCase()
+    }
 }
