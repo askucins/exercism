39b3944b539e9aec8f1dcfa29242d94a31ffa63e
Adam Skucinski <askucins@gmail.com>
2019-07-20 21:55:05 +0200

    applied misc changes after reviewing community solutions

--- groovy/isbn-verifier/src/main/groovy/IsbnVerifier.groovy
+++ groovy/isbn-verifier/src/main/groovy/IsbnVerifier.groovy
@@ -6,13 +6,13 @@ class IsbnVerifier {
             return false // ISBN has to have 10 characters
         }
         if (tokens =~ /[^X0-9]/) {
-            return false //only digits and X
+            return false // only digits and X are allowed
         }
         if (tokens[0..-2].contains('X')) {
-            return false //X only allowed on the last position
+            return false // X only allowed on the last position
         }
 
         def digits = tokens.collect { it == 'X' ? 10 : it.toInteger() }
-        [digits, 10..1].transpose().collect { it[0] * it[1] }.sum(0) % 11 == 0
+        [digits, 10..1].transpose().collect { a, b -> a * b }.sum(0) % 11 == 0
     }
 }
