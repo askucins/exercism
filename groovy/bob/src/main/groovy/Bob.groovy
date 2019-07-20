ee8dfac53d34a0111c79f50e1801c963b3fd90b9
Adam Skucinski <askucins@gmail.com>
2019-07-20 20:15:26 +0200

    passed tests of bob

--- groovy/bob/src/main/groovy/Bob.groovy
+++ groovy/bob/src/main/groovy/Bob.groovy
@@ -1,5 +1,53 @@
 class Bob {
+
+    static Boolean isQuestion(input) {
+        input ==~ /.*?\?\s*\z/
+    }
+
+    static Boolean isYelled(input) {
+        input ==~ /[^a-z]+/ && input =~ /[A-Z]+/
+    }
+
+    static Boolean isYelledQuestion(input) {
+        isYelled(input) && isQuestion(input)
+    }
+
+    static Boolean isEmpty(input) {
+        input ==~ /\s*/
+    }
+
+    //Wrapping-up with closures is mandatory!
+    //See e.g. https://mrhaki.blogspot.com/2009/08/groovy-goodness-switch-statement.html
+    /*
+        "Anything that implements the isCase() method can be used as a classifier.
+        Groovy already added an isCase() method to
+        Class (uses isInstance),
+        Object (uses (equals),
+        collections (uses contains)
+        and regular expressions (uses matches).
+        If we implement the isCase method in our own Groovy classes we can use it as a classifier as well.
+        Finally we can use a closure as a classifier.
+        The closure will be evaluated to a boolean value."
+     */
+
     static response(input) {
-        throw new UnsupportedOperationException('method not implemented.')
+        def result = ''
+        switch (input) {
+            case { isYelledQuestion(it) }:
+                result = "Calm down, I know what I'm doing!"
+                break
+            case { isYelled(it) }:
+                result = 'Whoa, chill out!'
+                break
+            case { isQuestion(it) }:
+                result = 'Sure.'
+                break
+            case { isEmpty(it) }:
+                result = 'Fine. Be that way!'
+                break
+            default:
+                result = 'Whatever.'
+        }
+        result
     }
 }
