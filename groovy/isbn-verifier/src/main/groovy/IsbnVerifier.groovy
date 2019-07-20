267fa6b0e85e98c7a2529ddd07952c218e8eec76
Adam Skucinski <askucins@gmail.com>
2019-07-20 21:49:48 +0200

    passed tests of isbn-verifier

--- groovy/isbn-verifier/src/main/groovy/IsbnVerifier.groovy
+++ groovy/isbn-verifier/src/main/groovy/IsbnVerifier.groovy
@@ -1,5 +1,18 @@
 class IsbnVerifier {
     static boolean isValid(String isbn) {
-        throw new UnsupportedOperationException('method not implemented.')
+        def tokens = isbn.replaceAll('-', '')
+
+        if (tokens.size() != 10) {
+            return false // ISBN has to have 10 characters
+        }
+        if (tokens =~ /[^X0-9]/) {
+            return false //only digits and X
+        }
+        if (tokens[0..-2].contains('X')) {
+            return false //X only allowed on the last position
+        }
+
+        def digits = tokens.collect { it == 'X' ? 10 : it.toInteger() }
+        [digits, 10..1].transpose().collect { it[0] * it[1] }.sum(0) % 11 == 0
     }
 }
