fef3071281921c84c1f752e9ce6b75214ab94a24
Adam Skucinski <askucins@gmail.com>
2019-07-21 11:58:53 +0200

    updated based on community solutions

--- groovy/nucleotide-count/src/main/groovy/NucleotideCount.groovy
+++ groovy/nucleotide-count/src/main/groovy/NucleotideCount.groovy
@@ -1,10 +1,9 @@
 class NucleotideCount {
 
     static count(String strand) {
-        if (strand =~ /[^ACGT]/) {
+        def graph = ['A': 0, 'C': 0, 'G': 0, 'T': 0].withDefault {
             throw new ArithmeticException()
         }
-        Map graph = ['A': 0, 'C': 0, 'G': 0, 'T': 0]
         strand.toList().each { graph[(it)]++ }
         graph
     }
