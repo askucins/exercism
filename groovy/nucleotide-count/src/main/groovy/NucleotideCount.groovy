421af8716c8713cdf9b038ef1f1e35e1bebd1728
Adam Skucinski <askucins@gmail.com>
2019-07-21 10:52:13 +0200

    passed tests of nucleotide-count

--- groovy/nucleotide-count/src/main/groovy/NucleotideCount.groovy
+++ groovy/nucleotide-count/src/main/groovy/NucleotideCount.groovy
@@ -1,6 +1,11 @@
 class NucleotideCount {
 
-    static count(strand) {
-        throw new UnsupportedOperationException('Method implementation is missing')
+    static count(String strand) {
+        if (strand =~ /[^ACGT]/) {
+            throw new ArithmeticException()
+        }
+        Map graph = ['A': 0, 'C': 0, 'G': 0, 'T': 0]
+        strand.toList().each { graph[(it)]++ }
+        graph
     }
 }
