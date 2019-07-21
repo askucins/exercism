4eebf32e62fb090936890c76ef8eb16bd95709a1
Adam Skucinski <askucins@gmail.com>
2019-07-21 13:16:02 +0200

    passed tests of series

--- groovy/series/src/main/groovy/Series.groovy
+++ groovy/series/src/main/groovy/Series.groovy
@@ -1,6 +1,13 @@
 class Series {
 
-    static slices(series, sliceLength) {
-        throw new UnsupportedOperationException('method not implemented.')
+    static slices(String series, Integer sliceLength) {
+        if (sliceLength < 1) {
+            throw new ArithmeticException()
+        }
+        def upperLimit = series.length() - sliceLength
+        if (upperLimit < 0) {
+            throw new ArithmeticException()
+        }
+        (0..upperLimit).collect { series.substring(it, it + sliceLength) }
     }
 }
