e477d6e629bcfafdf02e51680ac3d5294d254cff
Adam Skucinski <askucins@gmail.com>
2019-07-27 23:33:23 +0200

    passed tests of rotational-cipher

--- groovy/rotational-cipher/src/main/groovy/RotationalCipher.groovy
+++ groovy/rotational-cipher/src/main/groovy/RotationalCipher.groovy
@@ -1,4 +1,5 @@
 class RotationalCipher {
+    private static List<String> alphabet = ('a'..'z')
     private int key
 
     RotationalCipher(int key) {
@@ -6,6 +7,15 @@ class RotationalCipher {
     }
 
     String rotate(String cipherText) {
-        throw new UnsupportedOperationException('Method implementation is missing')
+        cipherText.toList().collect {
+            def result = it
+            def idx = alphabet.indexOf(it.toLowerCase())
+            if (idx >= 0) {
+                def position = (idx + key) % alphabet.size()
+                String rotated = alphabet[(position)]
+                result = it.toCharacter().lowerCase ? rotated : rotated.toUpperCase()
+            }
+            result
+        }.join()
     }
 }
