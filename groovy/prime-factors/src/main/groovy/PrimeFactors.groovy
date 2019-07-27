6de92514d23b16fe796613587b6b6db100c74860
Adam Skucinski <askucins@gmail.com>
2019-07-27 22:51:02 +0200

    passed all but last tests for prime-factors

--- groovy/prime-factors/src/main/groovy/PrimeFactors.groovy
+++ groovy/prime-factors/src/main/groovy/PrimeFactors.groovy
@@ -1,6 +1,14 @@
+import static org.apache.commons.math3.primes.Primes.primeFactors
+
+// I am a stinky cheater and I will use org.apache.commons.math3.primes.Primes.primeFactors
+//... unfortunately it fails on the 93819012551 (Long, not int!)
 class PrimeFactors {
 
-    static factors(value) {
-        throw new UnsupportedOperationException('Method implementation is missing')
+    static factors(int value) {
+        if (value < 2) {
+            return []
+        } else {
+            return primeFactors(value)
+        }
     }
 }
