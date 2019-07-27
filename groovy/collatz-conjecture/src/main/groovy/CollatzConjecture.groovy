b27436cbcb8f2263e60e724cc234439d6a4f8512
Adam Skucinski <askucins@gmail.com>
2019-07-27 19:22:05 +0200

    misc changes

--- groovy/collatz-conjecture/src/main/groovy/CollatzConjecture.groovy
+++ groovy/collatz-conjecture/src/main/groovy/CollatzConjecture.groovy
@@ -1,14 +1,11 @@
 class CollatzConjecture {
 
     static int steps(int number) {
-        int stepsMade = 0
         if (number < 1) {
             throw new ArithmeticException()
         }
-        while (true) {
-            if (number == 1) {
-                break
-            }
+        int stepsMade = 0
+        while (number != 1) {
             if (number % 2 == 0) {
                 number = number / 2
             } else {
