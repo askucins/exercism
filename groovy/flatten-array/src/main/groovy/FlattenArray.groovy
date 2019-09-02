b0c29680e55f29d226b1f6fa16f246ef71ed88a1
Adam Skucinski <askucins@gmail.com>
2019-09-02 20:31:07 +0200

    passed tests of flatten-array

--- groovy/flatten-array/src/main/groovy/FlattenArray.groovy
+++ groovy/flatten-array/src/main/groovy/FlattenArray.groovy
@@ -1,5 +1,24 @@
 class FlattenArray {
+
+    static List<Integer> flattener(List array, List<Integer> accu) {
+        if (array?.isEmpty()) {
+            accu
+        } else {
+            def head = array?.head()
+            List tail = array?.tail()
+            if (head == null) {
+                flattener(tail, accu)
+            } else if (head.toString().isInteger()) {
+                flattener(tail, accu << head.toInteger())
+            } else {
+                accu.addAll(flattener(head as List, []))
+                flattener(tail, accu)
+            }
+        }
+    }
+
     static List<Integer> flatten(List array) {
-        array.flatten().grep { it != null }*.toInteger()
+        //array.flatten().grep { it != null }*.toInteger()
+        flattener(array, [])
     }
 }
