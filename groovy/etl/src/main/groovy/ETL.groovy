26214443cd2e2a8d9b09f60a62330bf650e90abe
Adam Skucinski <askucins@gmail.com>
2019-09-02 23:11:09 +0200

    passed tests of etl

--- groovy/etl/src/main/groovy/ETL.groovy
+++ groovy/etl/src/main/groovy/ETL.groovy
@@ -1,6 +1,15 @@
+import java.util.Map.Entry
+
 class ETL {
 
     static Map transform(Map input) {
-        [:]
+        input.inject([:]) { output, Entry weightForLetters ->
+            String weight = weightForLetters.key
+            List<String> letters = weightForLetters.value as List
+            letters.each { letter ->
+                output[letter.toString().toLowerCase()] = weight.toInteger()
+            }
+            output as Map
+        }
     }
 }
