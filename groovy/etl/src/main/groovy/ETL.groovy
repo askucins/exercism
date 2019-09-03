f70944a39fc56b637d959a130df178d8af550937
Adam Skucinski <askucins@gmail.com>
2019-09-03 18:57:29 +0200

    added community-based solution for etl

--- groovy/etl/src/main/groovy/ETL.groovy
+++ groovy/etl/src/main/groovy/ETL.groovy
@@ -3,6 +3,12 @@ import java.util.Map.Entry
 class ETL {
 
     static Map transform(Map input) {
+        //transformMine(input)
+        transformMine2(input)
+        //transformCommunity(input)
+    }
+
+    static Map transformMine(Map input) {
         input.inject([:]) { output, Entry weightForLetters ->
             String weight = weightForLetters.key
             List<String> letters = weightForLetters.value as List
@@ -10,6 +16,22 @@ class ETL {
                 output[letter.toString().toLowerCase()] = weight.toInteger()
             }
             output as Map
+        }
+    }
+
+    static Map transformMine2(Map input) {
+        input.inject([:]) { output, weight, letters ->
+            letters.each { letter ->
+                output[letter.toString().toLowerCase()] = weight.toInteger()
+            }
+            output as Map
+        }
+    }
+    
+    // Based on community solutions
+    static Map transformCommunity(Map input) {
+        input.collectEntries { score, letters ->
+            letters.collectEntries { [(it.toString().toLowerCase()): score as Integer] }
         }
     }
 }
