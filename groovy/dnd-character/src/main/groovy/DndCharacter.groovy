c1775f654308109bf7add5a1c324edd8227f8047
Adam Skucinski <askucins@gmail.com>
2023-12-06 14:46:08 +0100

    completed exercise: dnd-character

--- groovy/dnd-character/src/main/groovy/DndCharacter.groovy
+++ groovy/dnd-character/src/main/groovy/DndCharacter.groovy
@@ -1,3 +1,47 @@
 class DndCharacter {
 
+    private static Random random = new Random()
+
+    private static Integer rollDice() {
+        random.nextInt(6) + 1
+    }
+
+    private static Integer initializeAbility() {
+        (1..4)
+            .collect { rollDice() }
+            .sort()
+            .reverse()
+            .take(3)
+            .sum()
+    }
+
+    // abilities
+    Integer strength
+    Integer dexterity
+    Integer constitution
+    Integer intelligence
+    Integer wisdom
+    Integer charisma
+
+    // character
+    Integer hitpoints
+
+    Integer ability() {
+        [strength, dexterity, constitution, intelligence, wisdom, charisma][rollDice() - 1]
+    }
+
+    Integer modifier(Integer ability) {
+        Math.floor((ability - 10) / 2).toInteger()
+    }
+
+    DndCharacter() {
+        strength = initializeAbility()
+        dexterity = initializeAbility()
+        constitution = initializeAbility()
+        intelligence = initializeAbility()
+        wisdom = initializeAbility()
+        charisma = initializeAbility()
+
+        hitpoints = 10 + modifier(constitution)
+    }
 }
