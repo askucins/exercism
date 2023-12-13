44aceeb604b3ef0aaa2dda1e4cf894a177e0a8a7
Adam Skucinski <askucins@gmail.com>
2023-12-13 02:15:09 +0100

    updated exercise: armstrong-numbers

--- groovy/armstrong-numbers/src/main/groovy/ArmstrongNumbers.groovy
+++ groovy/armstrong-numbers/src/main/groovy/ArmstrongNumbers.groovy
@@ -1,6 +1,7 @@
 class ArmstrongNumber {
 
     static isArmstrongNumber(Integer number) {
+        //Gotcha! 'toList()' converts the given CharSequence into a List of Strings of one character.
         def digits = number.toString().toList()*.toInteger()
         Integer numberLength = digits.size()
         number == digits.collect { Integer digit -> (digit**numberLength) }.sum(0)
