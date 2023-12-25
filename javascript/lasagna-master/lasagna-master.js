24d24157e42c468c8b5401315a812c329c311aa7
Adam Skucinski <askucins@gmail.com>
2023-12-25 21:41:58 +0100

    completed exercise: lasagna-master

--- javascript/lasagna-master/lasagna-master.js
+++ javascript/lasagna-master/lasagna-master.js
@@ -10,3 +10,84 @@
  *   ...
  * }
  */
+
+/**
+ * Calculates cooking status
+ *
+ * @param {number} timer
+ * @returns {string} cooking status
+ */
+export function cookingStatus (timer) {
+  let result
+  if (timer === 0) {
+    result = 'Lasagna is done.'
+  } else if (timer > 0) {
+    result = 'Not done, please wait.'
+  } else {
+    result = 'You forgot to set the timer.'
+  }
+  return result
+}
+
+/**
+ * Calculates preperation time for provided layers
+ *
+ * @param {string[]} layers
+ * @param {number} layerPreparationTime (default 2)
+ * @returns {number} total preparation time
+ */
+export function preparationTime (layers, layerPreparationTime = 2) {
+  return layers?.length * layerPreparationTime
+}
+
+/**
+ * Calculates quantities of sauce and noodles
+ *
+ * @param {string[]} layers
+ * @returns {object} quantities of sauce and noodles
+ */
+export function quantities (layers) {
+  const result = {
+    noodles: 0,
+    sauce: 0
+  }
+  for (const layer of layers) {
+    switch (layer) {
+      case 'noodles':
+        result.noodles += 50
+        break
+      case 'sauce':
+        result.sauce += 0.2
+        break
+    }
+  }
+  return result
+}
+
+/**
+ *  Adds secret ingredient
+ *
+ *  @param {string[]} friendsIngredients
+ *  @param {string[]} yoursIngredients
+ *  @returns {undefined} (modifies passed yoursIngredients array)
+ */
+export function addSecretIngredient (friendsIngredients, yourIngredients) {
+  const secretIngredient = friendsIngredients[friendsIngredients.length - 1]
+  yourIngredients.push(secretIngredient)
+}
+
+/**
+ * Scales recipe for more portions
+ *
+ * @param {object} recipe
+ * @param {number} numberOfPortions
+ * @returns {object} scaled recipe
+ */
+export function scaleRecipe (recipe, numberOfPortions) {
+  const scale = numberOfPortions / 2
+  const result = {}
+  for (const ingredient in recipe) {
+    result[ingredient] = recipe[ingredient] * scale
+  }
+  return result
+}
